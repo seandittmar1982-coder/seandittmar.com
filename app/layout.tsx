2697f2023550c595049612cb9b1b389341581a44
realseandittmar <sean.dittmar@icloud.com>
2026-04-07 18:22:29 +0000

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
+import "./globals.css"
+import Nav from "./components/Nav";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        <Nav />
+
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,10 @@
-import { getHello } from './actions/actions'
-import Link from 'next/link'
-import Hello from './components/hello'
-export default async function Home() {
-const resultFromServerAction = await getHello();
 
+export default async function Home() {
 
   return (
-    <div className="card flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black pt-2 px-4">
-      <div className="card">
-        <h1 className="card">Hello from server component <code>page.tsx</code></h1>
-        <p className='card'>Result from server action: <code>{resultFromServerAction}</code></p>
-      </div>
-      <div className="card">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-        <Hello  />
-
-      </div>
-      
-      <div className="card">
-        <p>This is the profile page of Sean Dittmar.</p>
-        <p><Link href="/">seandittmar.com</Link></p>
-        <p><Link href="https://github.com/seandittmar1982-coder/seandittmar">Github Code</Link></p>
-      </div>
-    </div>
+      Sean Dittmar
+    </main>
   );
 }
